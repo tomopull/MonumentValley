26763cdffb6b07fddf2147621e60a97575a4686f
tomopull <tomohiro.ohsawa@gmail.com>
2016-04-11 22:35:55 +0900

    zutoshomen

--- MonumentValley/Assets/Script/manager/GameObjectManager.cs
+++ MonumentValley/Assets/Script/manager/GameObjectManager.cs
@@ -19,97 +19,102 @@ public class GameObjectManager : MonoBehaviour {
 
 	private NavMeshAgent _agent;
 
+	//--------------------------------------------
+	private float mouseX;
+	private float mouseY;
+	private float cameraDif;
+	private Vector3 worldpos;
+	//--------------------------------------------
+	
+
 	public void Start(){
 		// 床面衝突用のFloorレイヤを取得し、レイヤマスクに変換
 		floorMask = 1 << LayerMask.NameToLayer("Floor");
 	}
 
+
 	public void SetRotationAngleByTargetPosition(GameObject _char, Animator _animator,  Vector3 _vec_3){
+
 		//マウスポインターが何らかのEventSystem関連のUI用のGameObject上になかった場合
 		if(!EventSystem.current.IsPointerOverGameObject()){
 
-
-			Vector3 selfScreenPoint = Camera.main.WorldToScreenPoint(_char.transform.position);
-
-			Vector3 diff = Input.mousePosition - selfScreenPoint;
-
-			float angle = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg;
-
+			Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+			Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+			lookPos = lookPos - _char.transform.position;
+			float angle = Mathf.Atan2(lookPos.z, lookPos.x) * Mathf.Rad2Deg;
 			float final_angle = angle -90f;
-
-			_animator.transform.eulerAngles = new Vector3(0,-final_angle* 0.8f,0);
-			//_animator.transform.eulerAngles = new Vector3(0,-final_angle,0);
-			
-			//_char.transform.eulerAngles = new Vector3(0,-final_angle,0);
-
-			Debug.Log(final_angle);
-		
-
-
-
-			//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			//RaycastHit hit = new RaycastHit();
-			
-//			if(Physics.Raycast(ray,out hit)){
-//
-//				string selected = hit.collider.gameObject.name;
-//				Quaternion _rotate = Quaternion.LookRotation((Input.mousePosition - selfScreenPoint)* Mathf.Deg2Rad);
-//				_rotate.x = _rotate.z = 0;
-//				_char.transform.rotation = _rotate;
-//			}
-
-			//Quaternion _rotate = Quaternion.LookRotation((Input.mousePosition - selfScreenPoint));
-
-			//_char.transform.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-
-			//_rotate.x = _rotate.z = 0;
-			//_char.transform.rotation = _rotate;
-			//_char.transform.rotation = Quaternion.Slerp(_char.transform.rotation,_rotate,Time.deltaTime * 1f);
-
-			//Debug.Log(_char.transform.rotation.y * Mathf.Rad2Deg);
-
-			//Debug.Log(Input.mousePosition + "mouse pos");
-			//Debug.Log(_char.transform.rotation + "rotate");
-
-//			float angle = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg;
-//			float final_angle = angle -90f;
-
-//			Quaternion _tmp_rotation = Quaternion.LookRotation(diff);
-//			_char.transform.rotation = _tmp_rotation;
-
-//			Quaternion rotate = Quaternion.LookRotation(target.transform.position - transform.position);
-//			rotate.x = rotate.z = 0;
-//			transform.rotation = rotate;
-
-
-
-
-//			Vector3 selfScreenPoint = Camera.main.WorldToScreenPoint(_char.transform.position);
-//			Vector3 diff =  Input.mousePosition - selfScreenPoint;
-//			float angle = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg;
-//			float final_angle = angle -90f;
-//			Debug.Log(_char.transform.rotation + "rotation");
-//			Debug.Log(final_angle + "final_angle");
-
-		
-
+			_char.transform.rotation = Quaternion.AngleAxis(final_angle, Vector3.down);
+			Debug.Log(_char.transform.rotation);
 		}
 
 	}
 
-
-
-//	public void SetRotationAngleByTargetPosition(GameObject _char,Vector3 _vec_3){
-//		//マウスポインターが何らかのEventSystem関連のUI用のGameObject上になかった場合
-//		if(!EventSystem.current.IsPointerOverGameObject()){
-//			Vector3 selfScreenPoint = Camera.main.WorldToScreenPoint(_char.transform.position);
-//			Vector3 diff = Input.mousePosition - selfScreenPoint;
-//			float angle = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg;
-//			float final_angle = angle -90f;
-//			_char.transform.eulerAngles = new Vector3(0,final_angle,0);
+//	public void SetRotationAngleByTargetPosition(GameObject _char, Animator _animator,  Vector3 _vec_3){
+//	
+//	//マウスポインターが何らかのEventSystem関連のUI用のGameObject上になかった場合
+//	if(!EventSystem.current.IsPointerOverGameObject()){
+//
+//			Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+//			Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+//			lookPos = lookPos - _hero.transform.position;
+//			float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+//			_hero.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+//
 //
 //		}
-//		
+//	
+//	}
+
+
+//	public void SetRotationAngleByTargetPosition(GameObject _char, Animator _animator,  Vector3 _vec_3){
+//
+//		//マウスポインターが何らかのEventSystem関連のUI用のGameObject上になかった場合
+//		if(!EventSystem.current.IsPointerOverGameObject()){
+//			cameraDif = Camera.main.transform.position.y - _hero.transform.transform.position.y;
+//			mouseX = Input.mousePosition.x;
+//			mouseY = Input.mousePosition.y;
+//			worldpos = Camera.main.ScreenToWorldPoint(new Vector3(mouseX, mouseY, cameraDif));
+//			Vector3 turretLookDirection = new Vector3 (worldpos.x,_hero.transform.position.y, worldpos.z);
+//			_hero.transform.LookAt(turretLookDirection);
+//		}
+//
+//	}
+	
+	
+	//	public void SetRotationAngleByTargetPosition(GameObject _char, Animator _animator,  Vector3 _vec_3){
+//		//マウスポインターが何らかのEventSystem関連のUI用のGameObject上になかった場合
+//		if(!EventSystem.current.IsPointerOverGameObject()){
+//
+//
+//			Vector3 selfScreenPoint = Camera.main.WorldToScreenPoint(_char.transform.position);
+//
+//			Vector3 diff = Input.mousePosition - selfScreenPoint;
+//
+//			float angle = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg;
+//
+//			float final_angle = angle -90f;
+//
+//			_animator.transform.eulerAngles = new Vector3(0,final_angle,0);
+//
+//			Debug.Log(final_angle);
+//
+//		}
+//
+//	}
+
+
+
+////	public void SetRotationAngleByTargetPosition(GameObject _char,Vector3 _vec_3){
+////		//マウスポインターが何らかのEventSystem関連のUI用のGameObject上になかった場合
+////		if(!EventSystem.current.IsPointerOverGameObject()){
+////			Vector3 selfScreenPoint = Camera.main.WorldToScreenPoint(_char.transform.position);
+////			Vector3 diff = Input.mousePosition - selfScreenPoint;
+////			float angle = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg;
+////			float final_angle = angle -90f;
+////			_char.transform.eulerAngles = new Vector3(0,final_angle,0);
+////
+////		}
+////		
 //	}
 
 
